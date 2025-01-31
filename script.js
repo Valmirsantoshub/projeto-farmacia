4362b8a58c3b5fa4d42763b842bddc80294bfaed
Valmirsantoshub <154386269+Valmirsantoshub@users.noreply.github.com>
2025-01-31 10:59:56 -0300

    alt

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+let carrinho = [];
 
+function addToCart(produto) {
+    // Adiciona o produto ao carrinho
+    carrinho.push(produto);
+    alert(`${produto} foi adicionado ao carrinho!`);
+}
+
+function redirecionarParaWhatsApp() {
+    if (carrinho.length === 0) {
+        // alert("Seu carrinho está vazio!");
+        return;
+    }
+    
+    let mensagem = "Olá, gostaria de comprar os seguintes produtos:%0A";
+    carrinho.forEach((produto, index) => {
+        mensagem += `%0A${index + 1}. ${produto}`;
+    });
+    
+    let numeroWhatsApp = "5515996846660";
+    let linkWhatsApp = `https://api.whatsapp.com/send?phone=${numeroWhatsApp}&text=${mensagem}`;
+    
+    window.open(linkWhatsApp, "_blank");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    let botaoWhatsApp = document.querySelector(".whatsapp-button");
+    botaoWhatsApp.addEventListener("click", redirecionarParaWhatsApp);
+});
