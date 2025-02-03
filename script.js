7d384649502bf83db7e9089cc81ab4a9d29b78f6
Valmirsantoshub <154386269+Valmirsantoshub@users.noreply.github.com>
2025-02-03 19:28:45 -0300

    alt

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-// let carrinho = [];
-
-// function addToCart(produto) {
-//     // Adiciona o produto ao carrinho
-//     carrinho.push(produto);
-//     alert(`${produto} foi adicionado ao carrinho!`);
-// }
-
-// function redirecionarParaWhatsApp() {
-//     if (carrinho.length === 0) {
-//         // alert("Seu carrinho está vazio!");
-//         return;
-//     }
-    
-//     let mensagem = "Olá, gostaria de comprar os seguintes produtos:%0A";
-//     carrinho.forEach((produto, index) => {
-//         mensagem += `%0A${index + 1}. ${produto}`;
-//     });
-    
-//     let numeroWhatsApp = "5515996846660";
-//     let linkWhatsApp = `https://api.whatsapp.com/send?phone=${numeroWhatsApp}&text=${mensagem}`;
-    
-//     window.open(linkWhatsApp, "_blank");
-// }
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     let botaoWhatsApp = document.querySelector(".whatsapp-button");
-//     botaoWhatsApp.addEventListener("click", redirecionarParaWhatsApp);
-// });
-
-// Mapeamento dos produtos: chave utilizada no onclick e seus respectivos dados
 const products = {
     'anticoncepcionais': { name: "Anticoncepcionais", price: 50.00 },
     'Repelentes': { name: "Repelentes", price: 25.00 },
